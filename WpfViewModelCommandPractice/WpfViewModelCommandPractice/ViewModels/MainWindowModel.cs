ddf499700102fb4ecf6f4b7b1a1b654c11375c31
むずでょ <muzudho1@gmail.com>
2021-09-15 01:06:26 +0900

    ViewCommandModel

---
+++ WpfViewModelCommandPractice/WpfViewModelCommandPractice/ViewModels/MainWindowModel.cs
@@ -0,0 +1,35 @@
+﻿namespace WpfViewModelCommandPractice.ViewModels
+{
+    using Livet.Commands;
+    using System;
+    using System.Diagnostics;
+
+    public class MainWindowModel
+    {
+        private ViewModelCommand _calledHello;
+
+        /// <summary>
+        /// ハローと表示するコマンド
+        /// </summary>
+        public ViewModelCommand CalledHello
+        {
+            get
+            {
+                if (_calledHello == null)
+                {
+                    _calledHello = new ViewModelCommand(Hello);
+                }
+                return _calledHello;
+            }
+        }
+
+        /// <summary>
+        /// ハローと表示します
+        /// </summary>
+        public void Hello()
+        {
+            // Console.WriteLine("Hello, world!!"); // デバッグ中は働かなかった
+            Trace.WriteLine("Hello, world!!"); // デバッグ中に働いた
+        }
+    }
+}
